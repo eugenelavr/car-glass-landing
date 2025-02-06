2cac3642b2794eace3868abf50aa8864bcfd1c8e
Yevhrnii Lavrinenkov <eugene.lavrinenkov@jsp.email>
2025-02-06 17:34:03 +0200

    changed structure

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <AppBar />
+
+    <v-main>
+      <LandingSection />
+
+      <LogoSection />
+
+      <WhatWeDoSection />
+
+      <DiscoverHelp />
+
+      <CustomizeSection />
+
+      <FunFactsSection />
+
+      <WhatPeopleSaySection />
+
+      <OurPortfolioSection />
+
+      <MeetOurTeamSection />
+
+      <AwardWinningSection />
+
+      <AmazingFeaturesSection />
+
+      <PricingSection />
+
+      <FAQSection />
+
+      <ContactUsSection />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
